1ba09a20bdb88a31c5bc5875127a6eb52b80ac56
shriniket <shriniketkulkarni42@gmail.com>
2024-05-22 09:07:53 +0530

    changes

--- pro2024/src/App.js
+++ pro2024/src/App.js
@@ -45,4 +45,5 @@ function App() {
 export default App;
 
 // export const baseURL = "http://localhost:3001";
-export const baseURL = "https://crypt-drive.cyclic.app";
+export const baseURL = "https://be-pro-mongodb.onrender.com";
+// export const baseURL = "https://crypt-drive.cyclic.app";
